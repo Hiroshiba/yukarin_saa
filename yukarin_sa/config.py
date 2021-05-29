9bc5a854b2ca8cc5f2daa466397886ab07f12987
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2021-05-29 19:03:59 +0000

    add step shift

--- yukarin_sa/config.py
+++ yukarin_sa/config.py
@@ -69,6 +69,7 @@ class TrainConfig:
     stop_iteration: int
     optimizer: Dict[str, Any]
     weight_initializer: Optional[str] = None
+    step_shift: Optional[Dict[str, Any]] = None
     num_processes: Optional[int] = None
     use_gpu: bool = True
     use_amp: bool = False
